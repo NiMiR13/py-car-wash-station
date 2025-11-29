62f4b7838277798b9a20d6d30c3f30450c93b53d
NiMiR13 <r0932665555@gmail.com>
2025-11-29 21:04:15 +0200

    Fix: Add default init values

---
+++ app/car_wash.py
@@ -0,0 +1,35 @@
+class Car:
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
+
+
+class CarWashStation:
+    def __init__(self, distance_from_city_center: float = 0.0, clean_power: float = 0.0, average_rating: float = 0.0, count_of_ratings: int = 0):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> float:
+        income = 0.0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return round(income, 1)
+        return income
+
+    def calculate_washing_price(self, car: Car) -> float:
+        rating_factor = self.average_rating / 3
+        distance_factor = self.distance_from_city_center / 5
+        return car.comfort_class * rating_factor * distance_factor
+
+    def wash_single_car(self, car: Car):
+        car.clean_mark = self.clean_power
+
+    def rate_service(self, rate: int):
+        total_rating = self.average_rating * self.count_of_ratings + rate
+        self.count_of_ratings += 1
+        self.average_rating = round(total_rating / self.count_of_ratings, 1)
