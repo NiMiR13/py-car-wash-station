af9f3616d72c880f14b06786f72d52afeb5dfad3
NiMiR13 <r0932665555@gmail.com>
2025-11-30 17:46:49 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -2,8 +2,14 @@
 from typing import List
 
 
-class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+class CarWashStation:
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: float,
+        average_rating: float,
+        count_of_ratings: int
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
