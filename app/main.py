bbc9a4f2a5425f68c20717fe0a017fac8fc3678b
NiMiR13 <r0932665555@gmail.com>
2025-11-30 10:40:06 +0200

    fix init and rate_service

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: float, average_rating: float, count_of_ratings: int, _=None):
+    def __init__(self, distance_from_city_center: float, clean_power: float, average_rating: float, count_of_ratings: int):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -29,7 +29,7 @@ class CarWashStation:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, rate: int):
-        total = self.average_rating * self.count_of_ratings + rate
+    def rate_service(self, rating: float):
+        total = self.average_rating * self.count_of_ratings + rating
         self.count_of_ratings += 1
         self.average_rating = round(total / self.count_of_ratings, 1)
