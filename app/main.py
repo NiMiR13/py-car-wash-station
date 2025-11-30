9ed7fa47fedb48c33aeb70a77f7ca32045cd99e6
NiMiR13 <r0932665555@gmail.com>
2025-11-30 10:36:38 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -6,11 +6,12 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: float, average_rating: float, count_of_ratings: int):
+    def __init__(self, distance_from_city_center: float, clean_power: float, average_rating: float, count_of_ratings: int, _=None):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
+        # ← вот эта строчка с , _=None — оставь её!
 
     def serve_cars(self, cars: list) -> float:
         income = 0.0
